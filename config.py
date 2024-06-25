9eaa7bdec8d58756b71c939ba8aeb418b4c4085b
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-06-25 12:09:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ API_ID = "23480585"
 API_HASH = "23af64c0a4ba8c1f0c04e9be07d0ba68"
 
 BOT_TOKEN = "7494096178:AAFnQJJyzv7cbzIw0dduh2mmxKzJ3JR1HXQ"
-OWNER_ID = "6441456023"
+OWNER_ID = "7306229091"
 
-MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
-MUST_JOIN = "dev_gagan"
+MONGO_DB_URI = "mongodb+srv://prakashsalvi2121:jD8FaX4dNhTNxct7@sring.t4s3gsn.mongodb.net/?retryWrites=true&w=majority&appName=sring"
+MUST_JOIN = "stringvala"
